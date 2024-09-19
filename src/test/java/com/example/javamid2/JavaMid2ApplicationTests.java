eb4cb37323f90aa428df7d1d8aa58f03572f4c09
Juyeonjoo <yjjjoo0331@gmail.com>
2024-09-19 18:04:17 +0900

    hello

---
+++ src/test/java/com/example/javamid2/JavaMid2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.javamid2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaMid2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
